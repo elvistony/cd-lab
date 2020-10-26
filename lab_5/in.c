5a44e4c7cbfbc33050143dc62811cab38084feb2
Elvis Tony <elvistonyrency@gmail.com>
2020-10-26 15:45:52 +0530

    Added Exp 5

---
+++ lab_5/in.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define max 10
+
+int main(){
+  char count;
+  int sums=1+2;  //Single Comment
+/*
+Multi line comment
+*/
+  if(1>2){
+    printf("True");
+  }
+  return 0;
+}
+
---
+++ lab_5/out.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+#define max 10
+ 
+
+ 
+int main(){
+ 
+  char count;
+ 
+  int sums= 1+ 2;  
+ 
+
+ 
+  if(1>2){
+ 
+    printf(True);
+ 
+  }
+ 
+  return 0;
+ 
+}
+ 
+
+ 
